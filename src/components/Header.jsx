6ab9d2f5611e38c6b4e2aee2e4031ddb9411d28b
chiaralle <chiaraallegrino@gmail.com>
2025-03-17 17:06:36 +0100

    add: milstone 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="nav-container">
+            <a className="" href="#" > Home </a>
+            <a className="" href="#">About</a>
+            
+        </div>  
+
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
