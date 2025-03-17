2d64d215f416e109dbd132226e76e468021b1ec1
chiaralle <chiaraallegrino@gmail.com>
2025-03-17 17:21:32 +0100

    add: fine Esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Header from './components/Header';
+import DefaultLayout from './layouts/DefaultLayout';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+import MoviePage from './pages/MoviePage';
 
 function App() {
   
 
   return (
-    <>
-    <Header />
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
